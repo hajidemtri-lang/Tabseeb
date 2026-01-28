ac40b0cfa0aef67ab522cb52dff68ac97af62cbc
hajidemtri-lang <hajidemtri@gmail.com>
2026-01-28 15:57:52 +0300

    Update Tepthon.py

--- Tepthon.py
+++ Tepthon.py
@@ -3,7 +3,7 @@ import subprocess
 import base64
 
 
-repo_encoded = "aHR0cHM6Ly9naXRodWIuY29tL21kYWgyNTE5LWJ5dGUvUmU="
+repo_encoded = "aHR0cHM6Ly9naXRodWIuY29tL2hhamlkZW10cmktbGFuZy9SZQ=="
 branch = "main"
 
 def run(cmd):
